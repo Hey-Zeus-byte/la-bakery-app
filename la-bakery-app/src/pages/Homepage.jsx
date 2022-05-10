b2775e165491366e752ac71b4af9293d2c82ce4b
Jesus V <heyzeus.1096@gmail.com>
2022-05-10 04:39:57 -0700

    added images

--- la-bakery-app/src/pages/Homepage.jsx
+++ la-bakery-app/src/pages/Homepage.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
+import sign from "../images/sign.png";
+import cover from "../images/cover1.png";
 
 const Homepage = () => {
   return (
     <div>
-      <h1 style={{color: "green", fontSize: "100px"}}>
-        Panaderia La Michoacana
+      <h1 style={{color: "black", fontSize: "50px"}}>
+        <img
+          alt="sign"
+          src={sign}
+          height="225"
+          style={{"border-radius": "25px"}}
+        />{" "}
+        <p>In Business for more than 15 years</p>
       </h1>
       <NavBar />
-      <div>Body content</div>
+      <div>
+        <img alt="cover" src={cover} width="100%" style={{}} />
+      </div>
       <Footer />
     </div>
   );
--- la-bakery-app/src/pages/Pan.jsx
+++ la-bakery-app/src/pages/Pan.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import NavBar from "../NavBar";
 import Footer from "../Footer";
+import chocoChip from "../images/choco-chip.png";
+import happyFace from "../images/happyface.png";
+import concha from "../images/miniconcha.png";
+import pinkCookie from "../images/pinkcookie.png";
+import whiteCookie from "../images/whitecookie.png";
+import chocoCupcake from "../images/choco-cupcake.png";
+import vanillaCupcake from "../images/vanilla-cupcake.png";
+import "../styles.css";
 
 const Homepage = () => {
   return (
@@ -10,6 +18,55 @@ const Homepage = () => {
       </h1>
       <NavBar />
       <div> pan content</div>
+      <div class="responsive">
+        <img src={chocoChip} alt="choco-chip" width="600" height="400" />
+        <p class="desc">Chocolate Chip Cookie</p>
+      </div>
+      <div class="responsive">
+        <img src={happyFace} alt="happyface" width="600" height="400" />
+        <p class="desc">Happy Face Cookie</p>
+      </div>
+      <div class="responsive">
+        <img src={whiteCookie} alt="white-cookie" width="600" height="400" />
+        <p class="desc">White Cookie</p>
+      </div>
+      <div class="responsive">
+        <img src={pinkCookie} alt="pink-cookie" width="600" height="400" />
+        <p class="desc">Pink Cookie</p>
+      </div>
+      <div class="responsive">
+        <img src={concha} alt="concha" width="600" height="400" />
+        <p class="desc">Concha Click on the image to view different colors</p>
+      </div>
+      <div class="responsive">
+        <img
+          src={vanillaCupcake}
+          alt="vanilla-cupcake"
+          width="600"
+          height="400"
+        />
+        <p class="desc">Vanilla Cupcake</p>
+      </div>
+      <div class="responsive">
+        <img src={chocoCupcake} alt="choco-cupcake" width="600" height="400" />
+        <p class="desc">Chocolate Cupcake</p>
+      </div>
+      {/* <div class="responsive">
+        <img src={chocoChip} alt="choco-chip" width="600" height="400" />
+        <p class="desc">Chocolate Chip Cookie</p>
+      </div>
+      <div class="responsive">
+        <img src={chocoChip} alt="choco-chip" width="600" height="400" />
+        <p class="desc">Chocolate Chip Cookie</p>
+      </div>
+      <div class="responsive">
+        <img src={chocoChip} alt="choco-chip" width="600" height="400" />
+        <p class="desc">Chocolate Chip Cookie</p>
+      </div>
+      <div class="responsive">
+        <img src={chocoChip} alt="choco-chip" width="600" height="400" />
+        <p class="desc">Chocolate Chip Cookie</p>
+      </div> */}
       <Footer />
     </div>
   );
